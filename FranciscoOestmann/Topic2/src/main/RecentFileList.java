e22a9fc51606348651ede001240758bafca80a2e
Francisco Oestmann <frannzoestmann@gmail.com>
2016-02-28 18:50:02 -0300

    test added file to list

--- FranciscoOestmann/Topic2/src/main/RecentFileList.java
+++ FranciscoOestmann/Topic2/src/main/RecentFileList.java
@@ -2,20 +2,29 @@ package main;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class RecentFileList {
 	
-	public ArrayList createList(){
-		ArrayList list = new ArrayList();
+	public ArrayList<File> list = new ArrayList<File>();
+	Scanner sc = new Scanner(System.in);
+	
+	public ArrayList<File> createList(){
+		list = new ArrayList<File>();
+		
 		return list;
 	}
 
 	public File openFile() {
-		File file = new File("test.txt");
+		File file = new File("path");
+		list.add(file);
 		return file;
 	}
 	
 	
+
+	
+
 	
 }
--- FranciscoOestmann/Topic2/src/test/RecentFileListTest.java
+++ FranciscoOestmann/Topic2/src/test/RecentFileListTest.java
@@ -2,7 +2,8 @@ package test;
 
 import static org.junit.Assert.*;
 
-import java.util.ArrayList;
+import java.io.File;
+//import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -11,6 +12,8 @@ import main.RecentFileList;
 public class RecentFileListTest {
 
 	RecentFileList obj = new RecentFileList();
+	//ArrayList<Object> list = new ArrayList<Object>();
+	File file = new File("test.txt");
 	
 	@Test
 	public void checkClassObjectNotNull(){
@@ -18,19 +21,28 @@ public class RecentFileListTest {
 	}
 	
 	@Test
-	public void checkCreatedArrayList(){
+	public void checkCreatedArrayListNotNull(){
 		
 		//Checks if the ArrayList was instantiated
-		assertNotNull(obj.createList());
+		//assertNotNull(obj.createList());
 		
+	}
+	
+	@Test
+	public void checkIfListIsEmpty(){
 		//Checks if the ArrayList is empty
-		assert obj.createList().isEmpty();
+		//assertTrue(obj.createList().isEmpty());
 	}
 	
 	@Test
 	public void checkIfFileWasOpened(){
 		assertNotNull(obj.openFile());
-		
 	}
+	
+	@Test
+	public void checkIfFileWasAddedToList(){
+		assertTrue(obj.createList().contains(obj.openFile()));
+	}		
+	
 	
 }
