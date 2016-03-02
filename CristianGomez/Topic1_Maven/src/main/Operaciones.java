2aec5c14bfa52265794144bda33290f337cd6f88
Cristian Adrian Gomez <Cadrian.Gomez@gmail.com>
2016-03-02 02:58:23 -0300

    subido Topic 1

---
+++ CristianGomez/Topic1_Maven/src/main/Operaciones.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Operaciones {
+
+    public int multiplicacion(int a, int b) {
+        return a * b;
+    }
+
+    public int suma(int a, int b) {
+        return a + b;
+    }
+
+    public int resta(int a, int b) {
+        return a - b;
+    }
+
+    public int division(int a, int b) {
+        return a / b;
+    }
+
+}
+	
