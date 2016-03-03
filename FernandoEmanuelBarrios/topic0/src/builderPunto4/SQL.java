33c605ce5897c19dd9f754c56a54ebf190669f19
choco <fernando.e.b.88@gmail.com>
2016-03-03 10:44:18 -0300

    intentando borrar archivos

--- FernandoEmanuelBarrios/topic0/src/builderPunto4/SQL.java
+++
@@ -1,9 +0,0 @@
-package builderPunto4;
-
-
-import java.sql.SQLException;
-
-public interface SQL {
-	public String conectar(String driver,String url,String usr,String pwd) throws SQLException,ClassNotFoundException;
-	
-}
